1e4a3a86bb9b0f99bdc8451c4b8f853d506beaa5
Feycry <78263543+Feycry@users.noreply.github.com>
2025-11-05 15:15:50 +0200

    Tehtävä 2

---
+++ Viikko2/nhl-reader/src/index.py
@@ -0,0 +1,27 @@
+import requests
+from player import Player
+
+def main():
+    url = "https://studies.cs.helsinki.fi/nhlstats/2024-25/players"
+    response = requests.get(url).json()
+
+    #print("JSON-muotoinen vastaus:")
+    #print(response)
+
+    players = []
+
+    for player_dict in response:
+        player = Player(player_dict)
+        players.append(player)
+
+    nationality = input("Input nationality: ")
+
+    print(f"Players with nationality \"{nationality}\":")
+
+    for player in players:
+        if player.nationality == nationality:
+            print(player)
+
+
+if __name__ == "__main__":
+    main()
